9a5f5153bc60e581d4b0bfe056d4e8bd1c48478a
Olorunfemi100 <olorunfemisalako@gmail.com>
2023-08-15 16:55:48 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Function that prints the alphabet - I sometimes suffer from insomnia.
+ * print_alphabet - I sometimes suffer from insomnia.
  * And when I can't fall asleep, I play what I call the alphabet game
  */
 void print_alphabet(void)
