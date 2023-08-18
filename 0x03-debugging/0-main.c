592234a3793312da080f016e727dbc9c0eef2c25
Olorunfemi100 <olorunfemisalako@gmail.com>
2023-08-18 16:45:52 -0700

    check

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,22 @@
 * main - Function for positive or negative
 * Return: Always 0
 */
+void positive_or_negative(int num);
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i = 0;
+    positive_or_negative(i);
+    return 0;
+}
+
+void positive_or_negative(int num)
+{
+    if (num > 0)
+        printf("%d is positive\n", num);
+    else if (num < 0)
+        printf("%d is negative\n", num);
+    else
+        printf("%d is zero\n", num);
 }
 
