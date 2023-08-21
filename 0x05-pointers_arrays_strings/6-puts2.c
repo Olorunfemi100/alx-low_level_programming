611bb258313f67aed8d3f718e12092666d41e66c
Olorunfemi100 <olorunfemisalako@gmail.com>
2023-08-21 11:23:01 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts2 - Write a function that prints every
+ * other character of a string, starting with
+ * the first character, followed by a new line.
+ * @str: string user input
+ * Return: print first char and new line
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+
+	int a = 0;
+
+	char *b = str;
+	int c;
+
+	while (*b != '\0')
+	{
+		b++;
+		length++;
+	}
+	a = length - 1;
+	for (c = 0 ; c <= b ; c++)
+	{
+		if (c % 2 == 0)
+		{
+			_putchar(str[c]);
+		}
+	}
+	_putchar('\n');
+}
