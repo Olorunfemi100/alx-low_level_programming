773397a23d1aa01b4d76e8c69fb3252ec4388004
Olorunfemi100 <olorunfemisalako@gmail.com>
2023-09-13 11:34:25 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that prints the opcodes of its own main function.
+ * @argc: arguments
+ * @argv: arguments
+ * Return: Return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, a;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (a = 0; a < bytes; a++)
+	{
+		if (a == bytes - 1)
+		{
+			printf("%02hhx\n", arr[a]);
+			break;
+		}
+		printf("%02hhx ", arr[a]);
+	}
+	return (0);
+}
