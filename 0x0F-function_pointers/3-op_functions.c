689b853f3f8c9e90e2fe0b08ec93ac88da29e678
Olorunfemi100 <olorunfemisalako@gmail.com>
2023-09-13 11:29:18 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - This file should contain the 5 following functions (not more):.
+ * @a: First number.
+ * @b: Second number.
+ * Return: Sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub -This file should contain the 5 following functions (not more):.
+ * @a: First number.
+ * @b: Second number.
+ * Return: Difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - This file should contain the 5 following functions (not more):.
+ * @a: First number.
+ * @b: Second number.
+ * Return: Product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - This file should contain the 5 following functions (not more):.
+ * @a: First number.
+ * @b: Second number.
+ * Return: Quotient of a and b.
+ */
+
+int op_div(int a, int b)
+
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - This file should contain the 5 following functions (not more):.
+ * @a: First number.
+ * @b: Second number.
+ * Return: remainder.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
